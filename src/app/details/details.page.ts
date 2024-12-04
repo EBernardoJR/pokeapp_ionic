1e6ec42cfa247ecca8b9bb8901dfd1dc8f6b7ba3
livia helena <liviahelena006@gmail.com>
2024-12-04 14:28:43 -0300

    pagina de detalhes

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-
+ // Dados do Pokémon
+  pokemon = {
+    name: 'charmander',
+    weight: 85,
+    stats: {
+      hp: 39,
+      attack: 52,
+      defense: 43,
+    },
+    moves: ['mega-punch', 'fire-punch', 'scratch'],
+    imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
